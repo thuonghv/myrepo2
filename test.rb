3ed54adf73b6769d275b901a577aa4c12c625aea
ThinTT <thintt@gmail.vn>
2020-10-01 09:25:29 +0000

    edit file test.rb

--- test.rb
+++ test.rb
@@ -2,10 +2,15 @@ class Menu
   attr_accessor :name
   attr_accessor :price
   def initialize (name:, price:)
+    @name = name
+    @price = price
   end
 end
 
 class Drink < Menu
   attr_accessor :size
-  
+  def initialize(size:)
+    super(name:, price:)
+    @size = size
+  end
 end
